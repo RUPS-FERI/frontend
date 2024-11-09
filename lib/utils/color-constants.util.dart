4d36a34887536da5b110b779d74bc1c1efd41fb9
ALjaz Kokol <aljaz.kokol@easy.bi>
2024-11-09 18:36:51 +0100

    feature: [#6] Added color constants

---
+++ lib/utils/color-constants.util.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorConstants {
+  const ColorConstants();
+  static const Color scaffoldBackgroundColor = Color(0xFFE6E6E6);
+  static const Color commonGray = Color(0xFFDCDCDC);
+  static const Color disabledText = Color(0xFF6A6A6A);
+  static const Color actionBlueColor = Color(0xFF1A5AFF);
+  static const Color actionBlueColorHover = Color(0xFF3B6BF6);
+  static const Color actionRedColor = Color(0xFFFF1A57);
+}
